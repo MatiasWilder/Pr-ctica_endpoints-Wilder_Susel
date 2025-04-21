0ada7f27d4bb332c1be91df72ee0cf1b7024a474
Iamatoast <neuyv@protonmail.com>
2025-04-21 09:50:24 -0300

    Extras terminados 8)

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 const port = 3000;
+
+let personas = [];
 
 app.get("/", (req, res) => {
     res.send("Bienvenido a mi servidor");
@@ -32,5 +35,24 @@ app.get("/materias", (req, res) => {
         {"nombre": "Lengua"}
     ]);
 });
+
+app.post("/agregarpersona", (req, res) => {
+    personas.push(req.body);
+    res.send("Agregado");
+});
+
+app.get("/mostrarpersonas", (req, res) => {
+    res.send(personas);
+});
+
+app.get("/borrarpersona/:nombre", (req, res) => {
+    let persona;
+    if((persona = personas.filter(el => el.Nombre == req.params.nombre).length) > 0){
+        personas.splice(personas.indexOf(persona), 1);
+        res.send("Persona eliminada");
+    } else {
+        res.send("Persona no encontrada");
+    }
+})
 
 app.listen(3000, () => console.log("Listening on port " + port));
