0a2b9f2add890331bb9175add89dba49eb48f53e
Iamatoast <neuyv@protonmail.com>
2025-04-21 08:28:10 -0300

    TP

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Bienvenido a mi servidor");
+});
+
+app.get("/saludo", (req, res) => {
+    res.send("¡Hola, mundo!");
+});
+
+app.get("/numero", (req, res) => {
+    res.send("42");
+});
+
+app.get("/usuario", (req, res) => {
+    res.send({
+        "nombre": "Ana",
+        "edad": 25
+    });
+});
+
+app.get("/productos", (req, res) => {
+    res.send(["Mouse", "Teclado", "Monitor"]);
+});
+
+app.get("/materias", (req, res) => {
+    res.send([
+        {"nombre": "Matemática"},
+        {"nombre": "Lengua"}
+    ]);
+});
+
+app.listen(3000, () => console.log("Listening on port " + port));
